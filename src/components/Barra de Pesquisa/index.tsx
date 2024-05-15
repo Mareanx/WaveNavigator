90152b56f16ca2836518264d7a2046012cbae75e
mareanx <mareanx26@gmail.com>
2024-05-15 19:38:11 -0300

    add style

--- src/components/Barra de Pesquisa/index.tsx
+++ src/components/Barra de Pesquisa/index.tsx
@@ -1,3 +1,7 @@
 import React, { FormEvent, useContext, useState } from "react"; //FormEvent -> é um tipo do ts para eventos de formularios ** useContext -> hook para acessar contextos //
 import { fiSearch } from "react-icons/fi";
 import SearchContext from "../../contexts/search-context";
+
+import { Container } from "./style";
+
+const;
---
+++ src/components/Barra de Pesquisa/style.ts
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+
+   height: 80px;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   font-family: Helvetica, Arial, sans-serif;
+
+   h1{
+    margin-top:
+    font-size:
+    font-weight: bold;
+   }
+
+   form{
+   display: flex;
+   height: 40px;
+   margin-top: 10px;
+ }
+
+  input{
+    width: 50vw;
+    margin-right: 20px;
+    border: 1px solid gray;
+    padding: 0 20px;
+    border-radius 5px;
+  }
+
+  textarea:focus{
+    input:focus{
+        outline: none;
+        border: 2px solid #6272a4 !important;
+
+    }
+
+    button{
+        width: 60px;
+        border: 0;
+        cursor: pointer;
+        color: #282a36;
+        border-radius: 5px;
+        background-color: #bd93f9;
+    }
+
+    button:focus{
+        outline: none !important;
+    }
+`;
